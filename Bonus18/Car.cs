548c52274729168981bcb595bc18f4a213d1cc75
campbell88 <shatharcampbell@gmail.com>
2017-04-21 16:22:07 -0400

    Created array for multiple cars and it works hooraygit add .

--- Bonus18/Car.cs
+++ Bonus18/Car.cs
@@ -8,5 +8,87 @@ namespace Bonus18
 {
     class Car
     {
+        private string MakeOfCar;
+        private string ModelOfCar;
+        private int YearOfCar;
+        private double PriceOfCar;
+
+        public string PropMakeOfCar
+        {
+            get
+            {
+                return MakeOfCar;
+            }
+
+            set
+            {
+                MakeOfCar = value;
+            }
+        }
+
+        public string PropModelOfCar
+        {
+            get
+            {
+                return ModelOfCar;
+            }
+
+            set
+            {
+                ModelOfCar = value;
+            }
+        }
+
+        public int PropYearOfCar
+        {
+            get
+            {
+                return YearOfCar;
+            }
+
+            set
+            {
+                YearOfCar = value;
+            }
+        }
+
+        public double PropPriceOfCar
+        {
+            get
+            {
+                return PriceOfCar;
+            }
+
+            set
+            {
+                PriceOfCar = value;
+            }
+        }
+
+
+        #region Constructors
+        //constructor with four arguements
+        public Car(string MakeOfCarInput,
+        string ModelOfCarInput,
+       int YearOfCarInput,
+       double PriceOfCarInput)
+    {
+        MakeOfCar = MakeOfCarInput;
+        ModelOfCar = ModelOfCarInput;
+        YearOfCar = YearOfCarInput;
+        PriceOfCar = PriceOfCarInput;
+
+    }
+    // no-arguement constructor
+    public Car() 
+    {
+        MakeOfCar = "---";
+        ModelOfCar = "---";
+        YearOfCar = 2000;
+        PriceOfCar = 1000;
+
+    }
+        #endregion
     }
 }
+
--- Bonus18/Program.cs
+++ Bonus18/Program.cs
@@ -10,6 +10,49 @@ namespace Bonus18
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the GC Motor admin console!\n");
+
+            Console.WriteLine("How many cars are you entering: ");
+
+            int sizeOfLot = int.Parse(Console.ReadLine());
+            Car[] CarsArray = new Car[sizeOfLot];
+            
+            for (int i = 0; i < sizeOfLot; i++)
+            {
+
+                Console.WriteLine($"Enter Make of Car No. {i + 1}: ");
+                string MakeofCar = Console.ReadLine();
+
+                Console.WriteLine($"Enter Model of Car No. {i + 1}: ");
+                string ModelofCar = Console.ReadLine();
+
+                Console.WriteLine($"Enter Year of Car No. {i + 1}: ");
+                int YearofCar = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enter Price of Car No. {i + 1}: ");
+                double PriceofCar = double.Parse(Console.ReadLine());
+
+                CarsArray[i] = new Car(MakeofCar, ModelofCar, YearofCar, PriceofCar);
+
+
+
+            }
+
+            for (int i = 0; i < sizeOfLot; i++)
+            {
+                Console.WriteLine(CarsArray[i].PropMakeOfCar);
+                Console.WriteLine(CarsArray[i].PropModelOfCar);
+                Console.WriteLine(CarsArray[i].PropYearOfCar);
+                Console.WriteLine(CarsArray[i].PropPriceOfCar);
+            }
+
+
+                //Car Car1 = new Car(MakeofCar, ModelofCar, YearofCar, PriceofCar);
+                //Console.WriteLine(Car1.PropMakeOfCar);
+                //Console.WriteLine(Car1.PropModelOfCar);
+                //Console.WriteLine(Car1.PropYearOfCar);
+                //Console.WriteLine(Car1.PropPriceOfCar);
+
+            }
         }
-    }
 }
